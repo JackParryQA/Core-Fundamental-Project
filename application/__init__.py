486db5c89f71ddc39abf6ff283b96a41ee70701a
Jack Parry <JParry@qa.com>
2021-05-21 13:59:53 +0100

    integration test passed

--- application/__init__.py
+++ application/__init__.py
@@ -3,11 +3,9 @@ from os import getenv
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///data.db'
-# app.config['SQLALCHEMY_DATABASE_URI'] = getenv('DATABASE_URI')
+app.config['SQLALCHEMY_DATABASE_URI'] = getenv('DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
-# app.config['SECRET_KEY'] = getenv('SECRET_KEY')
-app.config['SECRET_KEY'] = 'sfiunsioudhao98'
+app.config['SECRET_KEY'] = getenv('SECRET_KEY')
 
 db = SQLAlchemy(app)
 
